8ecfb4ade561b483c59b25519ae14e2bac4390cb
zhutchens <zhut654@gmail.com>
2025-01-29 04:10:58 -0500

    update evaluation script

--- evaluate.py
+++ evaluate.py
@@ -1,43 +1,41 @@
 import sys
-if len(sys.argv) <= 8 or len(sys.argv) >= 10:
-    print('usage: <openai_model> <openai_model_temperature> <sentence_transformer_model> <which_textbook> <which_chapters> <what_to_test> <num_to_generate> <threshold>')
+if len(sys.argv) <= 6 or len(sys.argv) >= 8:
+    print('usage: <llm_temperature> <sentence_transformer_model> <which_textbook> <what_to_test> <num_to_generate> <threshold>')
     print('---Argument explanations---')
-    print('\topenai_model: openai model to use')
-    print('\topenai_model_temperature: temperature to use with openai model')
+    print('\tllm_temperature: temperature to use with openai model')
     print('\tsentence_transformer_model: sentence transformer model to use for embeddings')
     print('\twhich textbook to use for evaluation, options: dsa_2214, dsa_6114, cs_3190')
-    print('\twhich_chapters: a selected range of chapters to use for evaluation (ex. 1,2,3,4 or 6,7,8,9,10')
     print('\twhat_to_test: what to test for evaluation from the following: concepts, outcomes, key_terms')
     print('\tnum_generated: number of generated concepts, outcomes, or key_terms to get')
-    print('\tthreshold: threshold number for test case to pass (0.0 through 1.0)')
+    print('\tthreshold: threshold for evaluation metrics to pass')
 
     sys.exit()
 
-openai_model = sys.argv[1]
-temp = float(sys.argv[2])
-st_model = sys.argv[3]
+temp = float(sys.argv[1])
+st_model = sys.argv[2]
 
-textbook = sys.argv[4]
+textbook = sys.argv[3]
 if textbook != 'dsa_6114' and textbook != 'cs_3190' and textbook != 'dsa_2214':
     print('invalid textbook arg. use dsa_2214, dsa_6114, or cs_3190')
     sys.exit()
-which_chapters = [int(string) for string in sys.argv[5].split(',')]
 
-testing = sys.argv[6]
+testing = sys.argv[4]
 if testing != 'concepts' and testing != 'outcomes' and testing != 'outcomes':
     print('invalid what_to_test arg. use concepts, outcomes, or key_terms')
     sys.exit()
 
-num_generated = int(sys.argv[7])
-threshold = float(sys.argv[8])
+num_generated = int(sys.argv[5])
+threshold = float(sys.argv[6])
+
 
 from src.extractor import relationExtractor
 from dotenv import load_dotenv
 from os import getenv, environ
-from deepeval.metrics import AnswerRelevancyMetric, ContextualPrecisionMetric, ContextualRecallMetric, FaithfulnessMetric, ContextualRelevancyMetric
-from src.metrics.SemanticSimilarity import SemanticSimilarity
+from deepeval.metrics import AnswerRelevancyMetric, ContextualPrecisionMetric, ContextualRecallMetric, FaithfulnessMetric
+from src.metrics import SemanticSimilarity
 import pandas as pd
 import os
+from src.llms import OpenAIModel, HuggingFaceLLM
 
 load_dotenv()
 link = getenv(textbook) # Testing 2214 data structures textbook here
@@ -131,22 +129,20 @@ elif textbook == 'cs_3190':
         'Cascading'
     ]
 
-chapters = chapters[which_chapters[0]:which_chapters[-1] + 1]
+chapters = chapters[6]
 CHUNK_SIZE = 500
 CHUNK_OVERLAP = 100
 
+data = pd.read_csv('data/dsa_clifford_a_shaffer_3_2_java.csv')
+actual = [data['concepts'][6]]
+
 extractor = relationExtractor(link, 
-                            chapters, 
+                            [chapters], 
                             CHUNK_SIZE, 
                             CHUNK_OVERLAP, 
-                            'DocumentEmbeddings', 
-                            '2214_embeddings',
-                            connection,
-                            token,
-                            reset = True,
-                            openai_model = openai_model,
-                            st_model = st_model,
-                            temp = temp)
+                            OpenAIModel(),
+                            )
+
 
 if testing == 'concepts':
     generated, retrieved = extractor.identify_concepts(num_generated)
@@ -155,30 +151,10 @@ elif testing == 'outcomes':
 else:
     generated, retrieved = extractor.identify_key_terms(num_generated)
 
-data = pd.read_csv('data/chp4.csv')
-data.columns = ['concept']
-
-if testing == 'concepts' or testing == 'key_terms':
-    concept_data = data['concept'].tolist()
-    actual = []
-    for string in concept_data:
-        words = string.split('->')
-        for word in words:
-            if word not in actual:
-                actual.append(word)
-
-else:
-    outcome_data = data['outcome'].tolist()
-    actual = []
-    for string in outcome_data:
-        words = string.split(';')
-        for s in words:
-            if s not in actual:
-                actual.append(s)
 
 actual *= len(chapters)
 
-metrics = [SemanticSimilarity(st_model = extractor.embedding_model), AnswerRelevancyMetric(), FaithfulnessMetric(), ContextualPrecisionMetric(), ContextualRecallMetric(), ContextualRelevancyMetric()]
+metrics = [SemanticSimilarity(st_model = extractor.embedding_model), AnswerRelevancyMetric(model = extractor.llm), FaithfulnessMetric(model = extractor.llm), ContextualPrecisionMetric(model = extractor.llm), ContextualRecallMetric(model = extractor.llm)]
 results = extractor.evaluate(testing, num_generated, actual, metrics = metrics)
 
 if not os.path.exists('./results'):
@@ -187,9 +163,9 @@ if not os.path.exists('./results'):
 else:
     os.chdir('./results/')
 
-with open(f'results_{textbook}_{testing}.txt', 'w') as f:
+with open(f'results_{textbook}_{testing}_{st_model}_{extractor.llm.get_model_name()}.txt', 'w') as f:
     f.write('-' * 20 + '\n')
-    f.write(f'OPENAI MODEL: {extractor.openai_model}\n')
+    f.write(f'OPENAI MODEL: {extractor.llm.get_model_name()}\n')
     f.write(f'TEMPERATURE: {temp}\n')
     f.write(f'SENTENCE TRANSFORMER: {extractor.embedding_model}\n')
     f.write(f'TEXTBOOK: {textbook}\n')
@@ -230,4 +206,4 @@ with open(f'results_{textbook}_{testing}.txt', 'w') as f:
     for k, v in averages.items():
         f.write(f'{k}: {v}\n')
 
-print(f'Evaluation complete. Results written to results/results_{textbook}_{testing}.txt')
+print(f'Evaluation complete. Results written to results/results_{textbook}_{testing}_{st_model}_{extractor.llm.get_model_name()}.txt')
